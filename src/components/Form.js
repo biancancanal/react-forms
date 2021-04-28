20fc842fa0c93167560f8dc2c5f6957099c93326
Bianca <biancancanal@gmail.com>
2021-04-28 11:19:16 -0500

    commit tree 91854118c750e23a0945b6798087fe456eee591c

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 class Form extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry"
+    firstName: "",
+    lastName: "",
+    submittedData: []
   }
 
   handleFirstNameChange = event => {
+    console.log("event.target.value", event.target.value)
+    console.log("state", this.state.firstName)
     this.setState({
       firstName: event.target.value
     })
@@ -18,11 +21,27 @@ class Form extends React.Component {
     })
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state)
+    //send data somewhere to be added to database
+    this.setState({
+      firstName: "",
+      lastName: ""
+    })
+  }
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName} />
+        <input type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName} />
+        <input type= "submit" />
       </form>
     )
   }
